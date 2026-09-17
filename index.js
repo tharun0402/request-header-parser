d48c91397e9489acd1dd2dcca9b6892bcc8ecb3c
tharun0402 <krishnantharun2@gmail.com>
2026-09-17 13:13:49 +0530

    Complete Request Header Parser Microservice

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
+
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
