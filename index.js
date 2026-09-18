9f2b1947055128e64031027dd2c27d62511b6ad1
tharun0402 <krishnantharun2@gmail.com>
2026-09-18 21:08:50 +0530

    Complete request header parser API

--- index.js
+++ index.js
@@ -8,18 +8,23 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/whoami", function (req, res) {
-  const ip =
-    req.headers["x-forwarded-for"]?.split(",")[0] ||
+  var ip =
+    req.headers["x-forwarded-for"]?.split(",")[0].trim() ||
     req.socket.remoteAddress ||
     req.ip;
 
+  var language = req.headers["accept-language"] || "";
+  var software = req.headers["user-agent"] || "";
+
   res.json({
     ipaddress: ip,
-    language: req.headers["accept-language"],
-    software: req.headers["user-agent"]
+    language: language,
+    software: software
   });
 });
 
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log(
+    "Your app is listening on port " + listener.address().port
+  );
 });
